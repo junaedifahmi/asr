291d594821484162835f8a7054da9d5a11470517
juunnn <junaedi.fahmi15081@student.unsika.ac.id>
2019-12-26 08:50:03 +0700

    Add filter untuk mengecualikan wav yang tidak memiliki txt

--- w2l/w2l_datapreparation.py
+++ w2l/w2l_datapreparation.py
@@ -18,38 +18,50 @@ start_time = time.time()
 parser = argparse.ArgumentParser(description="Dataset Creation for w2l")
 parser.add_argument("--src", help="Folder data akustik dan text")
 parser.add_argument("--to", help="Folder data untuk hasil")
-parser.add_argument("--split",help="Split ke train dan test dalam persen",default=30)
-parser.add_argument("--log", help="Simpan dimana lognya", default='./log/datapreparation')
+parser.add_argument(
+    "--split", help="Split ke train dan test dalam persen", default=30)
+parser.add_argument("--log", help="Simpan dimana lognya",
+                    default='./logdatapreparation')
 args = parser.parse_args()
 
 path = args.src
 to = args.to + '/'
 split = int(args.split)
 os.makedirs(to, exist_ok=True)
-logging.basicConfig(filename=args.log, filemode='w', format='[%(asctime)s] [%(levelname)s] [%(massage)s]')
+logging.basicConfig(filename=args.log, filemode='w',
+                    format='[%(asctime)s] [%(levelname)s] [%(massage)s]')
 
 #os.makedirs(to, exist_ok=True)
 
-files = glob.glob(path+"/**/*.wav",recursive=True)
-logging.info("Terdapat %i wav files ditemukan dari folder %s",len(files),path)
+files = glob.glob(path+"/**/*.wav", recursive=True)
+logging.info("Terdapat %i wav files ditemukan dari folder %s",
+             len(files), path)
 
+
+for file in files:
+    if not os.path.isfile(file.replace('txt', 'wav')):
+        files.remove(file)
 
 # In[70]:
+
 
 def getname(filename):
     return os.path.basename(filename).split(".")[0]
 
+
 def gettext(filename):
-    filename = filename.replace('wav','txt')
+    filename = filename.replace('wav', 'txt')
     with open(filename, 'r') as f:
         txt = f.readline()
-    return txt.replace('\n','')
+    return txt.replace('\n', '')
+
 
 def getabs(filename):
     return os.path.abspath(filename)
 
-def writefile(filename,datas):
-    with open(filename,'w') as f:
+
+def writefile(filename, datas):
+    with open(filename, 'w') as f:
         for data in datas:
             f.write(getname(data))
             f.write("\t")
@@ -59,12 +71,14 @@ def writefile(filename,datas):
             f.write("\t")
             f.write(gettext(data))
             f.write("\n")
+
+
 def cacah(kata):
     x = [c for c in kata]
     for i in range(len(x)):
-        try: 
+        try:
             y = x[i+1]
-        
+
             if x[i] == 'n' and y == 'y':
                 x[i] = 'ny'
                 x[i+1] = '#'
@@ -80,36 +94,32 @@ def cacah(kata):
             elif x[i] == 't' and y == 'h':
                 x[i] = 'th'
                 x[i+1] = '#'
-            elif x[i] not in list('abcdefghijklmnopqrstuvwxyz') and x[i] != '#' :
+            elif x[i] not in list('abcdefghijklmnopqrstuvwxyz') and x[i] != '#':
                 x[i] = '|'
         except:
             pass
     x = [c for c in x if c != '#']
-    
+
     return x
-            
 
 
 # In[82]:
 
-
 os.makedirs(to+'/lists/', exist_ok=True)
-if int(split) > 0 :
+if int(split) > 0:
     k = int(split/100*len(files))
-    test = random.sample(files,k)
-    train = [ f for f in files if f not in test]
-    writefile(to+'/lists/train.lst',train)
-    writefile(to+'/lists/test.lst',test)
+    test = random.sample(files, k)
+    train = [f for f in files if f not in test]
+    writefile(to+'/lists/train.lst', train)
+    writefile(to+'/lists/test.lst', test)
 else:
-    writefile(to+'/lists/train.lst',file)
+    writefile(to+'/lists/train.lst', files)
 
 
 # In[102]:
+os.makedirs(to+'/txt/', exist_ok=True)
 
-
-os.makedirs(to+'/txt/',exist_ok=True)
-
-text = open(to+'/txt/text.txt','w')
+text = open(to+'/txt/text.txt', 'w')
 
 lexicon = []
 for f in files:
@@ -126,13 +136,13 @@ tokens = set()
 
 
 # Write Lexicon file "<word> <spelling>" format
-with open( (to+"/txt/lexicon.txt"), 'w' ) as f:
+with open((to+"/txt/lexicon.txt"), 'w') as f:
     for l in lexicon:
         ch = cacah(l)
         tokens |= set(ch)
-        print(l,' '.join(ch))
+        print(l, ' '.join(ch))
         f.writelines(l+" "+' '.join(ch)+"\n")
-        
+
 
 try:
     tokens.remove(' ')
@@ -140,13 +150,11 @@ except:
     pass
 
 # Write tokens list
-with open ( (to+"/txt/tokens.txt")  , 'w' ) as f:
+with open((to+"/txt/tokens.txt"), 'w') as f:
     tokens.add("|")
     for c in tokens:
         f.writelines(c+"\n")
 
 
 elapsed_time = time.time() - start_time
-logging.info("Proses selesai dengan waktu",elapsed_time/60,"menit")
-
-
+logging.info("Proses selesai dengan waktu", elapsed_time/60, "menit")
